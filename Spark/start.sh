81f30d3b4ec84f9b689197bf212ffb12b361f05b
momoirox <majablgic505@gmail.com>
2023-04-26 18:38:21 +0200

    Updates /seminarski

--- Spark/start.sh
+++
@@ -1,5 +0,0 @@
-sudo docker exec -it spark-master pip3 install requests
-
-sudo docker exec -it spark-master ./spark/bin/spark-submit  ../data/app.py &
-sudo docker exec -it spark-master ./spark/bin/spark-submit  ../Producer/Batch/load_movies.py 
-
